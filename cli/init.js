ce44c650c953478c077960dc09755dbf45ee3b82
Yury Shapkarin <yury@shapkarin.me>
2019-04-03 17:47:08 +0300

    init

---
+++ cli/init.js
@@ -0,0 +1,60 @@
+/**
+ * Command Line Interface
+ * 
+ * TODO: generate constatns, actions, reducers independed
+ * and also all in one inquirer
+ */
+
+const program = require('commander');
+const inquirer = require('inquirer');
+// TODO: use other dependency, just adds color to the text for createConstants()
+const ora = require('ora');
+
+const generate = require('../lib/generate');
+
+program
+  .command('constants [folder]')
+  .alias('const')
+  .option('-p, --path', 'add namespace path/YOUR_CONST')
+  .action(function(folder, {  path  }) {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'path',
+          message: 'namespace path/YOUR_CONST',
+          paginated: true,
+          when: function() {
+            return path;
+          }
+        },
+        {
+          type: 'input',
+          name: 'constants',
+          message: 'separated by comma',
+          validate: function(input) {
+            // TODO: not starts from nubmer
+            return true;
+          }
+        }
+      ]).then(function(answers) {
+        generate.createConstants({
+          answers,
+          folder,
+          cb: function(status) {
+            if (status) {
+              const spinner = ora();
+              spinner.text =
+                'constants created successfully';
+              spinner.succeed();
+            }
+          }
+        });
+      });
+  });
+
+
+/**
+ * parse commander object
+ */
+program.parse(process.argv);
