62fe2882fcb5201811ba64ec1894f4881d3759da
Yury Shapkarin <yury@shapkarin.me>
2019-04-11 01:23:09 +0300

    example

---
+++ example/constants.js
@@ -0,0 +1,3 @@
+export const LOAD_START = 'example/LOAD_START';
+export const LOAD_SUCCESS = 'example/LOAD_SUCCESS';
+export const LOAD_ERROR = 'example/LOAD_ERROR';
---
+++ example/reducers.js
@@ -0,0 +1,26 @@
+import {
+  LOAD_START,
+  LOAD_SUCCESS,
+  LOAD_ERROR
+} from './constants';
+
+const initialState = {};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case LOAD_START:
+      return {
+        ...state
+      };
+    case LOAD_SUCCESS:
+      return {
+        ...state
+      };
+    case LOAD_ERROR:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
+}
