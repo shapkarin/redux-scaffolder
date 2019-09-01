c30b03f4bb32416ab71685c4c27a1b6734ed9322
Yury Shapkarin <yury@shapkarin.me>
2019-09-01 03:47:40 +0300

    wip

--- example/reducers.js
+++ example/reducers.js
@@ -8,18 +8,22 @@ const initialState = {};
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    
     case LOAD_START:
       return {
         ...state
       };
+    
     case LOAD_SUCCESS:
       return {
         ...state
       };
+    
     case LOAD_ERROR:
       return {
         ...state
       };
+    
     default:
       return state;
   }
